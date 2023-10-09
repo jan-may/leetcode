4c548c1f845af62a9151170b8bf03602e39e7b9b
Jan <78799602+jan-may@users.noreply.github.com>
2023-10-09 13:21:29 +0200

    add solution 2859

--- rust/src/solution/mod.rs
+++ rust/src/solution/mod.rs
@@ -53,3 +53,4 @@ mod s1550_three_consecutive_odds;
 mod s1539_kth_missing_positive_number;
 mod s0034_find_first_and_last_position_of_element_in_sorted_array;
 mod s1920_build_array_from_permutation;
+mod s2859_sum_of_values_at_indices_with_k_set_bits;
