ef614ed447764aaf756e68ce9e4c6f1d529f1f13
Jan May <jan_may@gmx.net>
2023-10-30 10:48:10 +0100

    add solution 0547

--- rust/src/solution/mod.rs
+++ rust/src/solution/mod.rs
@@ -149,3 +149,4 @@ mod s2678_number_of_senior_citizens;
 mod s2744_find_maximum_number_of_string_pairs;
 mod s2778_sum_of_squares_of_special_elements;
 mod s2894_divisible_and_non_divisible_sum_difference;
+mod s0547_number_of_provinces;
---
+++ rust/src/solution/s0547_number_of_provinces.rs
@@ -0,0 +1,100 @@
+/**
+ * [547] Number of Provinces
+ *
+ * There are n cities. Some of them are connected, while some are not. If city a is connected directly with city b, and city b is connected directly with city c, then city a is connected indirectly with city c.
+ * A province is a group of directly or indirectly connected cities and no other cities outside of the group.
+ * You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the i^th city and the j^th city are directly connected, and isConnected[i][j] = 0 otherwise.
+ * Return the total number of provinces.
+ *  
+ * <strong class="example">Example 1:
+ * <img alt="" src="https://assets.leetcode.com/uploads/2020/12/24/graph1.jpg" style="width: 222px; height: 142px;" />
+ * Input: isConnected = [[1,1,0],[1,1,0],[0,0,1]]
+ * Output: 2
+ *
+ * <strong class="example">Example 2:
+ * <img alt="" src="https://assets.leetcode.com/uploads/2020/12/24/graph2.jpg" style="width: 222px; height: 142px;" />
+ * Input: isConnected = [[1,0,0],[0,1,0],[0,0,1]]
+ * Output: 3
+ *
+ *  
+ * Constraints:
+ *
+ * 	1 <= n <= 200
+ * 	n == isConnected.length
+ * 	n == isConnected[i].length
+ * 	isConnected[i][j] is 1 or 0.
+ * 	isConnected[i][i] == 1
+ * 	isConnected[i][j] == isConnected[j][i]
+ *
+ */
+pub struct Solution {}
+
+// problem: https://leetcode.com/problems/number-of-provinces/
+// discuss: https://leetcode.com/problems/number-of-provinces/discuss/?currentPage=1&orderBy=most_votes&query=
+
+// submission codes start here
+
+struct UnionFind {
+    parent: Vec<usize>,
+    count: usize,
+}
+
+impl UnionFind {
+    fn new(n: usize) -> Self {
+        let mut parent = vec![0; n];
+        for i in 0..n {
+            parent[i] = i;
+        }
+        UnionFind { parent, count: n }
+    }
+
+    fn find(&mut self, x: usize) -> usize {
+        if self.parent[x] != x {
+            self.parent[x] = self.find(self.parent[x]);
+        }
+        self.parent[x]
+    }
+
+    fn union(&mut self, x: usize, y: usize) {
+        let root_x = self.find(x);
+        let root_y = self.find(y);
+        if root_x != root_y {
+            self.parent[root_x] = root_y;
+            self.count -= 1;
+        }
+    }
+}
+
+impl Solution {
+    pub fn find_circle_num(is_connected: Vec<Vec<i32>>) -> i32 {
+        // using union find
+        let mut uf = UnionFind::new(is_connected.len());
+        for i in 0..is_connected.len() {
+            for j in 0..is_connected.len() {
+                if is_connected[i][j] == 1 {
+                    uf.union(i, j);
+                }
+            }
+        }
+        uf.count as i32
+    }
+}
+
+// submission codes end
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_547() {
+        assert_eq!(
+            Solution::find_circle_num(vec![vec![1, 1, 0], [1, 1, 0], [0, 0, 1]]),
+            2
+        );
+        assert_eq!(
+            Solution::find_circle_num(vec![vec![1, 0, 0], [0, 1, 0], [0, 0, 1]]),
+            3
+        );
+    }
+}
