0d69ccf2142e7845e1ee36ffc5a96608eae72809
Jan May <jan_may@gmx.net>
2023-11-26 13:47:47 +0100

    add solution 0215

--- rust/src/solution/mod.rs
+++ rust/src/solution/mod.rs
@@ -208,3 +208,4 @@ mod s0075_sort_colors;
 mod s1561_maximum_number_of_coins_you_can_get;
 mod s1685_sum_of_absolute_differences_in_a_sorted_array;
 mod s1727_largest_submatrix_with_rearrangements;
+mod s0215_kth_largest_element_in_an_array;
